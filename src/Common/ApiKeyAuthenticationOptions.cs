eb9498c6b56b83a9dcb11ffc48d74f254c4c0548
Simon Novak <snovak7@gmail.com>
2023-07-09 22:19:44 +0200

    feature: Added ApiKeyAuthenticationOptions.cs

---
+++ src/Common/ApiKeyAuthenticationOptions.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) Escendit Ltd. All Rights Reserved.
+// Licensed under the MIT. See LICENSE.txt file in the solution root for full license information.
+
+namespace Escendit.Orleans.Clients.OpenSearch.Common;
+
+/// <summary>
+/// Api Key Authentication Options.
+/// </summary>
+public class ApiKeyAuthenticationOptions
+{
+    /// <summary>
+    /// Gets or sets the base64 encoded api key.
+    /// </summary>
+    /// <value>The base64 encoded api key.</value>
+    public string? Base64EncodedApiKey { get; set; }
+}
